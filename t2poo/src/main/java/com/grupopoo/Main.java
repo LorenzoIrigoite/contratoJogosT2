28621bc22dc605a5595a82034fe98b596ad3419a
Josias Rocha <josias.rocha@edu.pucrs.br>
2026-06-26 15:43:26 +0000

    Criacao classes views e ajuste na Main.java

--- t2poo/src/main/java/com/grupopoo/Main.java
+++ t2poo/src/main/java/com/grupopoo/Main.java
@@ -1,9 +1,12 @@
 package com.grupopoo;
-import com.grupopoo.app.ACMESpiele;
 
+import com.grupopoo.app.ACMESpiele;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ACMESpiele acmeSpiele = new ACMESpiele();
-        acmeSpiele.executar();
+        SpringApplication.run(Main.class, args);
     }
 }
