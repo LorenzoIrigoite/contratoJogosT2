5bb05205f13b4bbe1e670fb03a0f07ba7019acbf
Josias Rocha <josias.rocha@edu.pucrs.br>
2026-06-28 21:50:50 +0000

    Alterando o nome das classes View.

---
+++ t2poo/src/main/java/com/grupopoo/gui/GerenciamentoDadosView.java
@@ -0,0 +1,10 @@
+package com.grupopoo.gui;
+
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle ("Gerenciamento de Dados")
+@Route("gerenciamentoDados")
+public class GerenciamentoDadosView {
+
+}
